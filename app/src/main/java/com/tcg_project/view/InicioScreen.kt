52f6178bc12e0d9844bca0f0bc5a8e70c4562873
Cristian Orellana <cristianj.orellanaa@gmail.com>
2025-10-18 12:48:59 -0300

    configurando vistas de contacto y nosotros

--- app/src/main/java/com/tcg_project/view/InicioScreen.kt
+++ app/src/main/java/com/tcg_project/view/InicioScreen.kt
@@ -1,2 +1,50 @@
 package com.tcg_project.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Composable
+fun InicioScreen(
+    navController: NavController
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text("TCG-Project LOGO", fontSize = 24.sp)
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = { navController.navigate(AppScreen.Nosotros.route) }) {
+                Text("Nosotros")
+            }
+            Button(onClick = { navController.navigate(AppScreen.Contacto.route) }) {
+                Text("Contacto")
+            }
+            Button(onClick = { navController.navigate(AppScreen.Login.route) }) {
+                Text("Login")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Text("Productos", fontSize = 20.sp)
+        Button(onClick = { navController.navigate(AppScreen.Productos.route) }) {
+            Text("Ver todos los productos")
+        }
+
+    }
+}
--- app/src/main/java/com/tcg_project/view/NosotrosScreen.kt
+++ app/src/main/java/com/tcg_project/view/NosotrosScreen.kt
@@ -1,2 +1,17 @@
 package com.tcg_project.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NosotrosScreen() {
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text("Objetivo de la página")
+        Text("Dueños: Ignacio Osorio y Cristian Orellana")
+    }
+}
